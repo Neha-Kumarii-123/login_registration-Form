8f11e04e53540d3bd6c814a6912bdb68f68fea61
Neha Kumarii 123 <nknehakumariii810@gmail.com>
2025-07-22 21:37:22 +0500

    Reorganized project structure: added database/, includes/, public/ folders

---
+++ includes/config.example.php
@@ -0,0 +1,8 @@
+<?php
+// includes/config.example.php
+return [
+    'db_host' => 'YOUR_DB_HOST',
+    'db_user' => 'YOUR_DB_USERNAME',
+    'db_pass' => 'YOUR_DB_PASSWORD',
+    'db_name' => 'YOUR_DB_NAME'
+];
